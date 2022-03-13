4ff09ef0584e6ecf790e9f9c3cdef195e949a08b
RainPalmiano <100556863+RainPalmiano@users.noreply.github.com>
2022-03-13 22:21:40 +0800

    Add files via upload

--- BPSOScheduler_AppPrio/BatteryCode.m
+++ BPSOScheduler_AppPrio/BatteryCode.m
@@ -1,5 +1,4 @@
-function battcharge_t = BatteryCode(appsched, appwatt, PV, initial_charge, h_load)
-
+function battcharge_r = BatteryCode(appsched, appwatt, PV, initial_charge, h_load)
 %% Test Input
 t = 24; 
 batteryoperation=zeros(1,t);
@@ -7,41 +6,45 @@ batteryoperation=zeros(1,t);
 %% DATA INPUT
 tempvar = appsched.*appwatt;
 appenergy = sum(tempvar,1);
-imbalance = transpose(PV)-appenergy; 
+imbalance = transpose(PV)-appenergy;
 
 
-BattCharge = (initial_charge/100)*h_load;
-battcharge_t = zeros(1,t);
-HighThresh = 1*h_load;
+BattCharge = (initial_charge/100)*h_load; % initial charge
+battcharge_r = zeros(1,t);
+battcharge_t = zeros(1,t); % 24 hour battery current charge
+HighThresh = 1*h_load; 
 LowThresh = 0.4*h_load; 
 MaxBattCharge = 1*h_load;
 % MinBattCharge = 0; 
 MinBattCharge = LowThresh; 
 
 for a=1:t
-    if (HighThresh<=BattCharge)&&(BattCharge<=MaxBattCharge) %If greater than highthreshold and less than maxbatt charge
-        BattCharge = discharging(BattCharge,imbalance(a),MinBattCharge);
-        batteryoperation(a) = -1;
-    elseif (LowThresh<BattCharge)&&(BattCharge <HighThresh)
-        if (imbalance(a)>0)
-            BattCharge = charging(BattCharge,imbalance(a),MaxBattCharge);
-            batteryoperation(a) = 1;
-        elseif (imbalance(a)<0)
-            BattCharge = discharging(BattCharge,imbalance(a),MinBattCharge);
-            batteryoperation(a) = -1;
-        elseif (imbalance(a)==0)
-            batteryoperation(a)= 0;
+    if (HighThresh<BattCharge)&&(BattCharge<MaxBattCharge) %If greater than highthreshold and less than maxbatt charge
+        BattCharge = discharging(BattCharge,imbalance(1,a),MinBattCharge); % {Discharge}
+        batteryoperation(1,a) = -1;
+    elseif (LowThresh<=BattCharge)&&(BattCharge <=HighThresh) %if higher than low threshold and lower than high threshol 
+        if (imbalance(1,a)>0) % if PV-Appliance >0 {Charge}
+            BattCharge = charging(BattCharge,imbalance(1,a),MaxBattCharge);
+            batteryoperation(1,a) = 1;
+        elseif (imbalance(1,a)<0) % if Appliance > PV  {Discharge}
+            BattCharge = discharging(BattCharge,imbalance(1,a),MinBattCharge);
+            batteryoperation(1,a) = -1;
+        elseif (imbalance(1,a)==0) 
+            batteryoperation(1,a)= 0;
         end
-    elseif (BattCharge<=LowThresh)
-        if (imbalance(a)>0)
-            BattCharge = charging(BattCharge,imbalance(a),MaxBattCharge);
-            batteryoperation(a) = 1;
-        elseif (imbalance(a)<=0)
-            batteryoperation(a)= 0;
+    elseif (BattCharge<LowThresh) %If lower than low thresh
+        if (imbalance(1,a)>0) % If PV-App > 0 {Charge}
+            BattCharge = charging(BattCharge,imbalance(1,a),MaxBattCharge);
+            batteryoperation(1,a) = 1;
+        elseif (imbalance(1,a)<=0) % If PV <= App {Idle}
+            batteryoperation(1,a)= 0;
         end
     end
-    battcharge_t(1,a)=BattCharge;
+    battcharge_t(1,a)=BattCharge; % Save Battcharge
 end
+
+battcharge_r = diff([(initial_charge/100)*h_load battcharge_t]); %positive if charging, %negative if charging
+
 
 %% Charging Function Code
 
@@ -63,4 +66,7 @@ function EnergyDischarged = discharging(BattCharge,imbalance,MinBattCap)
 EnergyDischarged = NewBattCharge;
 end
 
+
+
 end
+
---
+++ BPSOScheduler_AppPrio/UserInput_Test_Ave.m
@@ -0,0 +1,22 @@
+%ID H# HT PT    TotW    B
+0  1      3  0  0   0
+%APP TW D ST ET R
+1  83.50  5  15 20 2
+3  38.66  2  15 16 8
+4  173.97 24 1  24 3
+5  220.75 5  18 2  9
+6  220.75 1  3  4  10
+7  70.11  24 1  24 1
+8  361.14 1  7  8  5
+8  361.14 1  14 15 5
+9  141.40 1  4  6  6 
+10 150.00 12 3  19 4
+22 152.00 1  3  4  7
+34 80.8   1  14 15 11
+%Battery (Ownership and Intial Charge) 
+98 1 80 0 0 0  
+%EV (Ownership and Intial Charge)
+99 1 60 0 0 0  
+%Solar (No. of Solar Panels)
+100 3 0 0 0 0 
+
