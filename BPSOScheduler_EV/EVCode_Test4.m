1b01304cdedf83f08943adcbcc431f2ca4bc634d
nomnom32 <100557990+nomnom32@users.noreply.github.com>
2022-03-15 21:48:48 +0800

    Add files via upload

--- BPSOScheduler_EV/EVCode_Test4.m
+++ BPSOScheduler_EV/EVCode_Test4.m
@@ -27,10 +27,10 @@ peak = max(kwH);
 tempvar = appsched.*appwatt;
 appenergy = sum(tempvar,1);
 imbalance = peak-appenergy; 
-limit=300;
 
 %e-trike
 ev_rating = 3300;
+limit=1200; %25A*48V
 %https://gulfnews.com/world/asia/philippines/900-e-trikes-deployed-in-manila-1.2230670
 
 %e-car
@@ -100,54 +100,69 @@ for a=1:12 %Removed the excess from the limits
     end
 end
 
-app_and_ev = [appenergy(1,1:2) appenergy(1,15:24)]+evcharge_t %ev charge + app energy, para macheck pa rin yung peak ng usage
-[x_s,x_s_i]=sort(app_and_ev,'ascend') %ascending order 6pm to 6am, sort from lowest to peak pero 6pm to 6am lang
-x_lay = diff([x_s x_s(1,12)]) %rate of stairs, difference nung sortation
+%% If There Are Excess
+    %which is least likely to occur because of the flattening of the
+    %charging time and BPSO optimization
+evcharge_t
+if removed>0
 
-
-for a=1:12 % for every x_layer
-
-    % If may ascending stair, and yung idadagdag is mas mababa sa removed (yung binawas)
-    if (x_lay(1,a)>0) && (x_lay(1,a)*(a-(sum(lim(1,1:a),2)))<removed) 
-        for k=1:a %for every element at that level (stair)
-            %If di malilimit, at di limited: add yung layer, bawas sa
-            %removed
-            if x_lay(1,a)+evcharge_t(1,x_s_i(1,k))<limit && not(lim(1,x_s_i(1,k))) 
-                evcharge_t(1,x_s_i(1,k))=evcharge_t(1,x_s_i(1,k))+x_lay(1,a); %add yung x_layer(1,a)
-                removed=removed-x_lay(1,a)
-            %If malilimit, at di limited: gawing limit, bawas sa removed, 
-            elseif x_lay(1,a)+evcharge_t(1,x_s_i(1,k))>limit && not(lim(1,x_s_i(1,k)))
-                evcharge_t(1,x_s_i(1,k))=limit; %gawing limited yung ev charge na yon
-                removed=removed-(limit-evcharge_t(1,x_s_i(1,k)))
-                lim(1,x_s_i(1,k))=1; %gawing limited yung ev charge na yon
+    app_and_ev = [appenergy(1,1:2) appenergy(1,15:24)]+evcharge_t; %ev charge + app energy, para macheck pa rin yung peak ng usage
+    [x_s,x_s_i]=sort(app_and_ev,'ascend'); %ascending order 6pm to 6am, sort from lowest to peak pero 6pm to 6am lang
+    x_lay = diff([x_s x_s(1,12)]) %rate of stairs, difference nung sortation
+    
+    ex_lim = zeros(1,12);
+    for a=1:12
+        ex_lim(a)=lim(x_s_i(a));
+    end
+    ex_lim
+    sum(ex_lim(1,1:a),2)
+    for a=1:12 % for every x_layer
+        a
+        % If may ascending stair, and yung idadagdag is mas mababa sa removed (yung binawas)
+        if (x_lay(1,a)>0) && (x_lay(1,a)*(a-(sum(ex_lim(1,1:a),2)))<removed && a>(sum(ex_lim(1,1:a),2))) 
+            for k=1:a %for every element at that level (stair)
+                %If di malilimit, at di limited: add yung layer, bawas sa
+                %removed
+                if x_lay(1,a)+evcharge_t(1,x_s_i(1,k))<limit && not(ex_lim(1,k)) 
+                    evcharge_t(1,x_s_i(1,k))=evcharge_t(1,x_s_i(1,k))+x_lay(1,a) %add yung x_layer(1,a)
+                    removed=removed-x_lay(1,a)
+                    1
+                %If malilimit, at di limited: gawing limit, bawas sa removed, 
+                elseif x_lay(1,a)+evcharge_t(1,x_s_i(1,k))>limit && not(ex_lim(1,k))
+                    removed=removed-(limit-evcharge_t(1,x_s_i(1,k)))
+                    evcharge_t(1,x_s_i(1,k))=limit; %gawing limited yung ev charge na yon
+                    ex_lim(1,k)=1; %gawing limited yung ev charge na yon
+                    2
+                end
             end
-        end
-
-     %if may ascending value, and yung idadagdag is mas mataas sa removed,
-     %yung removed value ang gagamitin pang add
-    elseif x_lay(1,a)>0 && (x_lay(1,a)*(a-sum(lim(1,1:a),2))) > removed
-        poss = a-sum(transpose(lim(1,1:a)),1); %yung value distribution ng removed, sa hindi pa limited na items
-        
-        for k=1:a %for every element sa level na yon
-          
-            if removed <=0 
-                %SHOULD NOT HAPPEN
-                break
-            %if removed is 0 pa rin, and yung idagdag ay hindi maglimit,
-            %add yung value distributed removed/poss
-            elseif removed/poss + evcharge_t(1,x_s_i(1,k)) < limit && not(lim(1,x_s_i(1,k))) 
-                evcharge_t(1,x_s_i(1,k))=evcharge_t(1,x_s_i(1,k))+removed/poss
-                removed=removed-removed/poss
-            %if malilimit: maging limit at ibawas
-            elseif removed/poss + evcharge_t(1,x_s_i(1,k)) > limit && not(lim(1,x_s_i(1,k))) %if sya malilimit, or di malilimit
-                evcharge_t(1,x_s_i(1,k))=limit
-                removed=removed-(limit-evcharge_t(1,x_s_i(1,k)))
+    
+         %if may ascending value, and yung idadagdag is mas mataas sa removed,
+         %yung removed value ang gagamitin pang add
+        elseif x_lay(1,a)>0 && (x_lay(1,a)*(a-sum(ex_lim(1,k),2))) > removed
+            poss = a-sum(ex_lim(1,1:a),2) %yung value distribution ng removed, sa hindi pa limited na items
+            
+            for k=1:a %for every element sa level na yon
+              
+                if removed <=0 
+                    %SHOULD NOT HAPPEN <0, only =0
+                    break
+                %if removed is 0 pa rin, and yung idagdag ay hindi maglimit,
+                %add yung value distributed removed/poss
+                elseif removed/poss + evcharge_t(1,x_s_i(1,k)) < limit && not(ex_lim(1,k)) 
+                    evcharge_t(1,x_s_i(1,k))=evcharge_t(1,x_s_i(1,k))+removed/poss
+                    removed=removed-removed/poss
+                    3
+                %if malilimit: maging limit at ibawas
+                elseif removed/poss + evcharge_t(1,x_s_i(1,k)) > limit && not(ex_lim(1,k)) %if sya malilimit, or di malilimit
+                    removed=removed-(limit-evcharge_t(1,x_s_i(1,k)))
+                    evcharge_t(1,x_s_i(1,k))=limit
+                    4
+                end
             end
         end
     end
-
-
 end
+
 
 %% Plotting
 evcharge_t = [evcharge_t(1,1:2) zeros(1,12) evcharge_t(1,3:12)]
