4891be7cc48c9cd97766c449b5abcb8aa820e498
RainPalmiano <100556863+RainPalmiano@users.noreply.github.com>
2022-03-23 12:42:53 +0800

    Add files via upload

---
+++ BPSOScheduler_AppPrio/UserInput_MostProb.m
@@ -0,0 +1,20 @@
+%ID H# HT PT    TotW    B
+0  1 3 10660.71 13325.89 126.85
+%APP TW D ST ET R
+1  83.50  12 15 3  1
+3  38.66  2  15 16 8
+4  289.95 24 1  24 3
+6  207.25 1  3  4  9
+7  70.11  24 1  24 2
+8  361.14 1  8  9  5
+8  361.14 1  15 16 5
+9  141.40 2  5  7  6 
+10 180.00 12 5  17 4
+22 152.00 1  4  5  7
+34 80.8   1  15 16 10
+%Battery (Ownership and Intial Charge) 
+98 1 40 0 0 0  
+%EV (Ownership and Intial Charge)
+99 1 20 0 0 0  
+%Solar (No. of Solar Panels)
+100 3 0 0 0 0 
